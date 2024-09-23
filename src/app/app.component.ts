6b09ba15aa64c31c6c7178889b857ead4fb4733a
Ani0769 <w0769190@myscc.ca>
2024-09-23 00:42:08 -0400

    done with first assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Anilkumar-Learning-Angular';
+  aboutme = 'I am Anil Kumar'
+    course = 'Computer Programming'
 }
