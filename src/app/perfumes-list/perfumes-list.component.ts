4a1a3dff7d8edecb8b95e4d85b330fbe50b119a8
Ani0769 <w0769190@myscc.ca>
2024-09-25 00:51:20 -0400

    edit html file in perfume list

--- src/app/perfumes-list/perfumes-list.component.ts
+++ src/app/perfumes-list/perfumes-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Perfumes} from "../Shared/perfumes";
 import {PerfumesListItemComponent} from "../perfumes-list-item/perfumes-list-item.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-perfumes-list',
   standalone: true,
   imports: [
-    PerfumesListItemComponent
+    PerfumesListItemComponent,
+    NgForOf
   ],
   templateUrl: './perfumes-list.component.html',
   styleUrl: './perfumes-list.component.css'
