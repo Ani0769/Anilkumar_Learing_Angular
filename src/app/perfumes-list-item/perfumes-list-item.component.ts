7414947c36ec885f91d4b4e65ef79c93881d146a
Ani0769 <w0769190@myscc.ca>
2024-09-25 12:22:35 -0400

    done with assignment

--- src/app/perfumes-list-item/perfumes-list-item.component.ts
+++ src/app/perfumes-list-item/perfumes-list-item.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
+import {NgIf} from "@angular/common";
 
 
 @Component({
   selector: 'app-perfumes-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './perfumes-list-item.component.html',
   styleUrl: './perfumes-list-item.component.css'
 })
