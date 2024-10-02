a94d9026a5cf1eca17075cbec6e66d7b97cb2f38
Ani0769 <w0769190@myscc.ca>
2024-10-02 12:42:20 -0400

    injected dependency in app component and and added a perfume list component in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Perfumes } from "./Shared/perfumes";
-import {JsonPipe, NgForOf} from "@angular/common";
+import {JsonPipe, NgForOf, NgIf} from "@angular/common";
 import {PerfumesListComponent} from "./perfumes-list/perfumes-list.component";
+import {PerfumesListItemComponent} from "./perfumes-list-item/perfumes-list-item.component";
+import {PerfumeService} from "./services/perfume.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, JsonPipe, PerfumesListComponent],
+  imports: [RouterOutlet, NgForOf, JsonPipe, PerfumesListComponent, PerfumesListItemComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Royal Perfumes';
+  selectedperfumes?: Perfumes;
+
+  constructor(private PerfumeService : PerfumeService){}
+
+  ngOnInit(): void {
+
+    this.PerfumeService.getPerfumeById(3).subscribe(perfumes =>{
+      this.selectedperfumes = perfumes;
+    })
+  }
+
 
 }
 
--- src/app/perfumes-list/perfumes-list.component.ts
+++ src/app/perfumes-list/perfumes-list.component.ts
@@ -31,4 +31,6 @@ export class PerfumesListComponent {
       this.perfumes = data;
     });
   }
+
+
 }
