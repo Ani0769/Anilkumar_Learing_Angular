a1c0d2fcf44af45cda190e1a0eb13f4a848594ab
Ani0769 <w0769190@myscc.ca>
2024-10-01 23:34:38 -0400

    step 8 , 9 done

--- src/app/perfumes-list/perfumes-list.component.ts
+++ src/app/perfumes-list/perfumes-list.component.ts
@@ -3,6 +3,7 @@ import {Perfumes} from "../Shared/perfumes";
 import {PerfumesListItemComponent} from "../perfumes-list-item/perfumes-list-item.component";
 import {NgForOf} from "@angular/common";
 import {PerfumeService} from "../services/perfume.service";
+import {perfumesList} from "../data/mock-content";
 
 @Component({
   selector: 'app-perfumes-list',
@@ -24,4 +25,10 @@ export class PerfumesListComponent {
 
   perfumes: Perfumes[] = [];
   constructor(private perfumeService: PerfumeService) {}
+
+  ngOnInit(): void {
+    this.perfumeService.getPerfumes().subscribe((data: Perfumes[]) => {
+      this.perfumes = data;
+    });
+  }
 }
--- src/app/services/perfume.service.ts
+++ src/app/services/perfume.service.ts
@@ -7,10 +7,38 @@ import {Perfumes} from "../Shared/perfumes";
   providedIn: 'root'
 })
 export class PerfumeService {
+  private perfumes: Perfumes[] = perfumesList;
 
   constructor() { }
 
   getPerfumes(): Observable<Perfumes[]> {
-    return of(perfumesList);
+    return of(this.perfumes);
+  }
+
+  getPerfumeById(id: number): Observable<Perfumes | undefined> {
+    const perfume = this.perfumes.find(p => p.id === id);
+    return of(perfume);
+  }
+
+  addPerfume(perfume: Perfumes): Observable<Perfumes[]> {
+    this.perfumes.push(perfume);
+    return of(this.perfumes);
+  }
+
+  updatePerfume(updatedPerfume: Perfumes): Observable<Perfumes[]> {
+    const index = this.perfumes.findIndex(p => p.id === updatedPerfume.id);
+    if (index !== -1) {
+      this.perfumes[index] = updatedPerfume;
+    }
+    return of(this.perfumes);
+  }
+
+  removePerfume(id: number): Observable<Perfumes | null> {
+    const index = this.perfumes.findIndex(p => p.id === id);
+    if (index !== -1) {
+      const removedPerfume = this.perfumes.splice(index, 1)[0];
+      return of(removedPerfume);
+    }
+    return of(null);
   }
 }
