ce25de8862a20de0c60d7b24bec80055de6620e8
Ani0769 <w0769190@myscc.ca>
2024-10-01 23:16:01 -0400

    made mockcontent and service files in step 5

---
+++ src/app/data/mock-content.ts
@@ -0,0 +1,10 @@
+import {Perfumes} from "../Shared/perfumes";
+
+export const perfumesList: Perfumes[] =
+  [
+    {id: 1, name: "Oud wood", brand: "Tom ford", price: 400, description: "woody and strong", available: true},
+    {id: 2, name: "Le Male", brand: "Jean Paul Gaultier", price: 200, description: "mild and seduction", available: true},
+    {id: 3, name: "Spice Bomb", brand: "Viktor and Rolf", price: 200, description: "amber and strong", available: true},
+    {id: 4, name: "Homme", brand: "Dior", price: 160, description: "woody taste", available: true},
+    {id: 5, name: "Man in Black", brand: "Bvlgari", price: 300, description: "fragnance of flower with agarwood", available: false}
+  ]
--- src/app/perfumes-list/perfumes-list.component.ts
+++ src/app/perfumes-list/perfumes-list.component.ts
@@ -14,14 +14,7 @@ import {NgForOf} from "@angular/common";
   styleUrl: './perfumes-list.component.css'
 })
 export class PerfumesListComponent {
-  perfumesList: Perfumes[] =
-    [
-      {id: 1, name: "Oud wood", brand: "Tom ford", price: 400, description: "woody and strong", available: true},
-      {id: 2, name: "Le Male", brand: "Jean Paul Gaultier", price: 200, description: "mild and seduction", available: true},
-      {id: 3, name: "Spice Bomb", brand: "Viktor and Rolf", price: 200, description: "amber and strong", available: true},
-      {id: 4, name: "Homme", brand: "Dior", price: 160, description: "woody taste", available: true},
-      {id: 5, name: "Man in Black", brand: "Bvlgari", price: 300, description: "fragnance of flower with agarwood", available: false}
-    ]
+
   selectedPerfumes?: Perfumes;
   //function to set which student to display
   selectPerfumes(perfumes: Perfumes): void {
