f6ea5c1ff01f2e07a290bb2670c3a1547fc96dc0
Ani0769 <w0769190@myscc.ca>
2024-10-08 21:27:02 -0400

    assignment 3 complete step 11 12 done

--- src/app/perfumes-list-item/perfumes-list-item.component.ts
+++ src/app/perfumes-list-item/perfumes-list-item.component.ts
@@ -1,17 +1,19 @@
 import {Component, Input} from '@angular/core';
-import {NgIf} from "@angular/common";
+import {NgIf, NgStyle} from "@angular/common";
 
 
 @Component({
   selector: 'app-perfumes-list-item',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    NgStyle
   ],
   templateUrl: './perfumes-list-item.component.html',
   styleUrl: './perfumes-list-item.component.css'
 })
 export class PerfumesListItemComponent {
   @Input() perfumes?: any;
+  @Input() index!: number;
 
 }
