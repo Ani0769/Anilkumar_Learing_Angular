d2b0577069e7516d936e8b28e174d12256643436
Ani0769 <w0769190@myscc.ca>
2024-10-08 22:16:48 -0400

    assignment4 complete and did step 10 to 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,28 @@ import { RouterOutlet } from '@angular/router';
 import { Perfumes } from "./Shared/perfumes";
 import {JsonPipe, NgForOf} from "@angular/common";
 import {PerfumesListComponent} from "./perfumes-list/perfumes-list.component";
+import {PerfumesListItemComponent} from "./perfumes-list-item/perfumes-list-item.component";
+import {PerfumesService} from "./service/perfumes.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, JsonPipe, PerfumesListComponent],
+  imports: [RouterOutlet, NgForOf, JsonPipe, PerfumesListComponent, PerfumesListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Royal Perfumes';
+  perfume?: Perfumes;
+
+  constructor(private PerfumesService:PerfumesService ) {}
+
+  ngOnInit(): void {
+
+    this.PerfumesService.getPerfumeById(3).subscribe((data) => {
+      this.perfume = data;
+    });
+  }
 
 }
 
