65d63bc96dc67790d2384bd336380e59eabb4025
Ani0769 <w0769190@myscc.ca>
2024-10-11 01:29:36 -0400

    done with step 3 and 4 and 5.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter,Routes} from '@angular/router';
+import {PerfumesListComponent} from "./app/perfumes-list/perfumes-list.component";
+import {PerfumesListItemComponent} from "./app/perfumes-list-item/perfumes-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes=[
+  {path:'', redirectTo: '/perfumes', pathMatch: 'full'},
+  {path: 'perfumes' , component: PerfumesListComponent},
+  {path: 'perfumes/:id' , component: PerfumesListItemComponent},
+
+]
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
